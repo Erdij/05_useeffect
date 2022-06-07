7d5cb6c41e42a464fc6b604efe5d2f586771dc8c
Erdij <yildirimsukruerdi@gmail.com>
2022-06-07 20:52:55 +0300

    didMount

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 class ClassComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { count: 0 };
+  // }
+  //constructor olmadan da aşağıdaki gibi state tanımlayabiliriz.
+  state = { count: 0 };
+
+  increase = () => this.setState({ count: this.state.count + 1 });
+  decrease = () => this.setState({ count: this.state.count - 1 });
+  hello = () => this.setState({ say: "Hi" });
+
+  componentDidMount() {
+    console.log("Class Comp Mounted 2");
+    this.increase(); //component render olduktan sonra DidMount çalıştı ve sayacı bir arttırdı.
+    this.hello(); //aynı şekilde render olduktan sonra burası doğuyor.
   }
+
   render() {
+    console.log("Class Comp Rendered 1");
     return (
       <div className="class">
         <h2>Class Component</h2>
         <h3>count:{this.state.count}</h3>
+        <button onClick={this.increase}>Increase</button>
+        <button onClick={this.decrease}>Decrease</button>
+        <h3> Say : {this.state.say}</h3>
       </div>
     );
   }
