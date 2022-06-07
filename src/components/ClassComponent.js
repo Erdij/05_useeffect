d7ec7ede5ef9d97ccdbe79fb9deaf73d32db72be
Erdij <yildirimsukruerdi@gmail.com>
2022-06-07 19:19:57 +0300

    second

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
   render() {
     return (
       <div className="class">
         <h2>Class Component</h2>
+        <h3>count:{this.state.count}</h3>
       </div>
     );
   }
