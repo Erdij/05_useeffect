70225043954a95173497e892cf32329cc3441deb
Erdij <yildirimsukruerdi@gmail.com>
2022-06-07 23:38:59 +0300

    toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ClassComponent from "./components/ClassComponent";
+import React, { useState } from "react";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true); //#şarta bağlı render için.
   return (
     <div className="App">
-      <ClassComponent />
+      <button onClick={() => setIsVisible(!isVisible)}>Toggle Comp</button>
+      {isVisible && <ClassComponent />}
     </div>
   );
 }
